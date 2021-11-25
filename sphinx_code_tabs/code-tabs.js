597d72b0ae57a3b805e82615250576b3e86b802e
Robin De Schepper <robin.deschepper93@gmail.com>
2021-11-25 21:12:17 +0100

    Terminal JS & toggle whole page

--- sphinx_code_tabs/code-tabs.js
+++ sphinx_code_tabs/code-tabs.js
@@ -1,31 +1,30 @@
 const onclick = function(event) {
-	const select = this.parentNode;
-	const book = select.parentNode;
-	for (const button of select.children) {
-    button.classList.toggle('selected', Object.is(button, this));
+	for (const book of document.querySelectorAll('div.code-tabs')) {
+	  for (const page of book.children) {
+	    if (page.hasAttribute('data-id')) {
+	      page.classList.toggle('hidden', page.dataset.id != this.dataset.id);
+			}
+	  }
 	}
-  for (const page of book.children) {
-    if (page.hasAttribute('data-id')) {
-      page.classList.toggle('hidden', page.dataset.id != this.dataset.id);
-		}
-  }
 };
 
 window.addEventListener('load', function() {
   for (const book of document.querySelectorAll('div.code-tabs')) {
     let i = 0;
-    let navbar = document.createElement('ul');
+    let navbar = document.createElement('div');
+		navbar.classList.add("code-tabs-menu");
+		navbar.innerHTML = "•••";
     for (const page of book.children) {
-      const button = document.createElement('li');
+      const button = document.createElement('div');
       page.setAttribute('data-id', i);
 			button.setAttribute('data-id', i);
       button.onclick = onclick;
       button.innerText = page.getAttribute('data-title');
-      button.classList.toggle('selected', i == 0);
+			button.classList.add("code-tabs-menu-button");
       page.classList.toggle('hidden', i != 0);
       navbar.appendChild(button);
       ++i;
     }
-    book.prepend(navbar);
+		book.prepend(navbar);
   }
 }, false);
