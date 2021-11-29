422605f9e3319110abb4e56fcb0966f05f390234
Robin De Schepper <robin.deschepper93@gmail.com>
2021-11-29 12:48:47 +0100

    added ripple

--- sphinx_code_tabs/code-tabs.js
+++ sphinx_code_tabs/code-tabs.js
@@ -1,4 +1,8 @@
 const onclick = function(event) {
+	for (const btn of document.querySelectorAll('div.code-tabs-outer-button')) {
+	  console.log(btn.dataset, btn);
+    btn.classList.toggle("selected", btn.dataset.id == this.dataset.id);
+	}
 	for (const book of document.querySelectorAll('div.code-tabs')) {
 	  for (const page of book.children) {
 	    if (page.hasAttribute('data-id')) {
@@ -13,14 +17,23 @@ window.addEventListener('load', function() {
     let i = 0;
     let navbar = document.createElement('div');
 		navbar.classList.add("code-tabs-menu");
-		navbar.innerHTML = "•••";
+		navbar.innerHTML = "<div class='code-tabs-menu-controls'>•••</div>";
     for (const page of book.children) {
       const button = document.createElement('div');
+      if (i == 0) {
+        button.classList.add("selected");
+      }
+      const innerButton = document.createElement('div');
+      const rippleButton = document.createElement('div');
+      button.appendChild(innerButton);
+      button.appendChild(rippleButton);
       page.setAttribute('data-id', i);
 			button.setAttribute('data-id', i);
+      button.classList.add("code-tabs-outer-button");
       button.onclick = onclick;
-      button.innerText = page.getAttribute('data-title');
-			button.classList.add("code-tabs-menu-button");
+      innerButton.innerText = page.getAttribute('data-title');
+			innerButton.classList.add("code-tabs-menu-button");
+			rippleButton.classList.add("code-tabs-ripple-button");
       page.classList.toggle('hidden', i != 0);
       navbar.appendChild(button);
       ++i;
